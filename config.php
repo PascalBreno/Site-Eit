778d6670d4aa7e0868ac116e65c01c36146646cc
Unknown <brenodml@live.com>
2018-01-30 09:57:39 -0300

    Importando o projeto
    Não houve alterações, apenas importação do projeto todo para uma pasta do servidor local.

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+    //Dados da Conexão com o Banco Local
+    $host = "localhost";
+    $database = "eit";
+    $username = "Pascal";
+    $password = "1KGCVA62LXq";
+
+    //Criando a conexão com os dados anteriores
+    $conn = mysqli_connect($host, $username, $password, $database);
+
+?>
---
+++ index.php
@@ -0,0 +1,58 @@
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Escritório de Inovação e Tecnologia</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/cover.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center">
+
+    <div class="cover-container d-flex h-100 p-3 flex-column">
+      <header class="masthead mb-auto">
+        <div class="inner">
+          <h3 class="masthead-brand">EIT</h3>
+          <nav class="nav nav-masthead ">
+            <a class="nav-link active" href="#">Inicio</a>
+            <a class="nav-link" href="#">Contato</a>
+          </nav>
+        </div>
+      </header>
+
+      <main role="main" class="inner cover">
+        <h1 class="cover-heading">Escritório de Inovação e Tecnologia</h1>
+        <p class="lead"> O Escritório de Inovação Tecnológica (EIT) é o Núcleo de Inovação Tecnológica da Universidade Federal de Mato Grosso, criado pela Resolução CD nº 18/2007 e atualizado por meio da Resolução CD nº 17/2016 em consonância com o Novo Marco Legal da Inovação conforme disposto na Lei nº 13.243/2016. ​</p>
+        <p class="lead">
+          <a href="http://www.ufmt.br/ufmt/un/eit" target="_blank" class="btn btn-success">Ler mais</a>
+          <a href="Professor.php" class ="btn btn-primary">Entrar </a>
+        </p>
+      </main>
+
+      <footer class="mastfoot mt-auto">
+        <div class="inner">
+          <p>Escritório de Inovação e Tecnologia , by @BrenoFelix.</p>
+        </div>
+      </footer>
+    </div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages lo ad faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="js/vendor/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  <div style='text-align: right;position: fixed;z-index:9999999;bottom: 0; width: 100%;cursor: pointer;line-height: 0;'><a title="Hosted on free web hosting 000webhost.com. Host your own website for FREE." target="_blank" href="https://www.000webhost.com/?utm_source=000webhostapp&amp;utm_campaign=000_logo&amp;utm_medium=website_{$app['name']}&amp;utm_content=footer_img"><img src="https://cdn.rawgit.com/000webhost/logo/e9bd13f7/footer-powered-by-000webhost-white2.png"  alt="www.000webhost.com"></a></div></body>
+</html>
