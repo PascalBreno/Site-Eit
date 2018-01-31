317de274fade5893fc1e08a17782f29615d5546b
Unknown <brenodml@live.com>
2018-01-31 10:51:46 -0300

    Novo projeto
    Precisa arrumar o problema com o css...

--- index.php
+++ index.php
@@ -1,57 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <title>escritório de inovação e tecnologia</title>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+  <link rel="stylesheet"  href="css/bootstrap.min.css" />
+  <link href="csss/cover.css"  rel="stylesheet">  
 
-    <title>Escritório de Inovação e Tecnologia</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
-    <link href="css/cover.css" rel="stylesheet">
-  </head>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">EIT</a>
 
-  <body class="text-center">
-
-    <div class="cover-container d-flex h-100 p-3 flex-column">
-      <header class="masthead mb-auto">
-        <div class="inner">
-          <h3 class="masthead-brand">EIT</h3>
-          <nav class="nav nav-masthead ">
-            <a class="nav-link active" href="#">Inicio</a>
-            <a class="nav-link" href="#">Contato</a>
-          </nav>
-        </div>
-      </header>
-
-      <main role="main" class="inner cover">
-        <h1 class="cover-heading">Escritório de Inovação e Tecnologia</h1>
-        <p class="lead"> O Escritório de Inovação Tecnológica (EIT) é o Núcleo de Inovação Tecnológica da Universidade Federal de Mato Grosso, criado pela Resolução CD nº 18/2007 e atualizado por meio da Resolução CD nº 17/2016 em consonância com o Novo Marco Legal da Inovação conforme disposto na Lei nº 13.243/2016. ​</p>
-        <p class="lead">
-          <a href="http://www.ufmt.br/ufmt/un/eit" target="_blank" class="btn btn-success">Ler mais</a>
-          <a href="Professor.php" class ="btn btn-primary">Entrar </a>
-        </p>
-      </main>
-
-      <footer class="mastfoot mt-auto">
-        <div class="inner">
-          <p>Escritório de Inovação e Tecnologia , by @BrenoFelix.</p>
-        </div>
-      </footer>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="http://www.ufmt.br/eit/">Site do EIT</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contato</a>
+        </li>
+      </ul>
     </div>
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages lo ad faster -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="js/vendor/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-  </html>
+  </nav>
+    </head>
+    <body>
+        <div class="input-group input-group-lg" id ="pesquisa">
+        <input type="text" class="form-control" placeholder="Faça sua pesquisa aqui" aria-label="Large" aria-describedby="inputGroup-sizing-sm">
+        <button type="button" class="btn btn-xs btn-primary">Pesquisar </button>
+      </div>
+    </body>
+</html>
