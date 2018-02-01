8b25b6b6dbe5d5e7b95e40f2aa692f9295a4f336
Unknown <brenodml@live.com>
2018-02-01 11:37:53 -0300

    Novas mudanças
    Nessa etapa eu refiz todo o processo de pesquisa. Proxima etapa é adicionar as pesquisas pelo banco de dados.

--- index.php
+++ index.php
@@ -10,17 +10,17 @@
 
 
   <link rel="stylesheet"  href="css/bootstrap.min.css" />
-  <link href="csss/cover.css"  rel="stylesheet">  
+  <link href="css/style.css"  rel="stylesheet">  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    </head>
+    <body>
 
-
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+<!-- Menu, não mudar entre as novas abas -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light " >
     <a class="navbar-brand" href="#">EIT</a>
-
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
-    </button>
-    
+    </button>    
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item active">
@@ -35,11 +35,25 @@
       </ul>
     </div>
   </nav>
-    </head>
-    <body>
-        <div class="input-group input-group-lg" id ="pesquisa">
-        <input type="text" class="form-control" placeholder="Faça sua pesquisa aqui" aria-label="Large" aria-describedby="inputGroup-sizing-sm">
-        <button type="button" class="btn btn-xs btn-primary">Pesquisar </button>
-      </div>
+
+
+<!-- Conteúdo da página em questão, usar para diferenciar entre ambas-->
+      <form "form-inline" action="pesquisa.php">
+        <div class="row">
+          <div class="col col-md-2"></div>
+          <div class=" col col-md-8" >
+            <div class="input-group input-group-lg col col-md-10" style="padding:20px 50px 0px 50px;">
+              <input type="text" class="form-control" placeholder="Faça sua pesquisa aqui" aria-label="Large" aria-describedby="inputGroup-sizing-sm">
+              <div class="col col-md-2">
+                <button type="button" class="btn btn-xs btn-primary botao "><span class="glyphicon glyphicon-search" aria-hidden="true"></span> Pesquisar </button>
+              </div>
+            </div>
+          </div>  
+          <div class="col col-md-2"> </div>    
+        </div>
+      </form>
+
+
+
     </body>
 </html>
